db5d640d81fc4e63e07068043eab0bbb810f919e
David Franco Ospina <david.franco@correounivalle.edu.co>
2023-01-09 13:55:13 -0500

    Changes in modal AddItems, added axios post request and modified componentes in the modal

--- src/routes/Items/ItemsManagement.jsx
+++ src/routes/Items/ItemsManagement.jsx
@@ -9,9 +9,14 @@ import {
 } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import AddItem from "./Modals/AddItem";
 
 const ItemsManagement = () => {
   const [items, setItems] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
 
   useEffect(() => {
     axios
@@ -26,45 +31,47 @@ const ItemsManagement = () => {
   }, []);
 
   return (
-    <Container className="container-box shadow-sm">
-      <Row>
-        <Col>
-          <h5>Manejo de inventario</h5>
-        </Col>
-        <Col xl lg="1">
-          {/* TODO:Buscador */}
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        </Col>
-        <Col md="auto">
-          {/* TODO:Boton de agregar item */}
-          <Button variant="secondary">Agregar ítem</Button>
-        </Col>
-        <Col md="auto">
-          {/* TODO:Boton de importar items */}
-          <Button variant="dark">Agregar ítems</Button>
-        </Col>
-      </Row>
-      <Table striped bordered hover className="mt-2">
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>Nombre</th>
-            <th>Cantidad</th>
-            <th>Precio unidad</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((item) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.name}</td>
-              <td>{item.quantity}</td>
-              <td>{item.unit_price}</td>
+    <>
+      <AddItem show={showModal} onHide={handleClose}></AddItem>
+      <Container className="container-box shadow-sm">
+        <Row>
+          <Col>
+            <h5>Manejo de inventario</h5>
+          </Col>
+          <Col xl lg="100" md="auto">
+            {/* TODO:Buscador */}
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          </Col>
+          <Col md="auto">
+            <Button variant="secondary" onClick={handleShow}>Agregar ítem</Button>
+          </Col>
+          <Col md="auto">
+            {/* TODO:Boton de importar items */}
+            <Button variant="dark">Agregar ítems</Button>
+          </Col>
+        </Row>
+        <Table striped bordered hover className="mt-2">
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>Nombre</th>
+              <th>Cantidad</th>
+              <th>Precio unidad</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
-    </Container>
+          </thead>
+          <tbody>
+            {items.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.quantity}</td>
+                <td>{item.unit_price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+    </>
   );
 };
 
---
+++ src/routes/Items/Modals/AddItem.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { Modal, Form, Button } from "react-bootstrap";
+import axios from 'axios';
+
+const AddItem = (props) => {
+
+    const [formData, setFormData] = useState({});
+
+    const handleChangeField = (event) => {
+        setFormData({
+            ...formData,
+            [event.target.id]: event.target.value
+        });
+    }
+
+    const handleCreateItem = (event) =>{
+        event.preventDefault();
+
+        axios.post('http://192.168.1.26:3000/Item/createNew', formData)
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+  return (
+    <Modal show={props.show} onHide={props.onHide} className="custom-modal">
+      <Modal.Header closeButton>
+        <Modal.Title>Agregar item</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleCreateItem}>
+          <Form.Group className="mb-3" controlId="name">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control type="text" placeholder="Escribe el nombre" onChange={handleChangeField} />
+            <Form.Text className="text-muted">
+              Nombre con descripción detallada del producto.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="quantity">
+            <Form.Label>Cantidad</Form.Label>
+            <Form.Control type="text" placeholder="Ingresa la cantidad" onChange={handleChangeField} />
+            <Form.Text className="text-muted">
+              Cantidad del componente.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="price">
+            <Form.Label>Precio</Form.Label>
+            <Form.Control type="text" placeholder="Precio unitario" onChange={handleChangeField} />
+            <Form.Text className="text-muted">
+              Ingresa el precio por unidad.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Categoría</Form.Label>
+            <Form.Select>
+                <option>TODO:map para traer todos los registros de categorias</option>
+            </Form.Select>
+            <Form.Text className="text-muted">
+              Selecciona la categoría a la que pertenece el ítem.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Tipo</Form.Label>
+            <Form.Select>
+                <option>TODO:map para traer todos los registros de tipos</option>
+            </Form.Select>
+            <Form.Text className="text-muted">
+              Selecciona el tipo al que pertenece el ítem.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-5">
+            <Form.Label>Propiedades</Form.Label>
+            <Form.Control
+                as="textarea"
+                placeholder="Ingresa las propiedades TODO: aquí debe aparecer un template predefinido"
+                style={{ height: '200px' }}
+            />
+          </Form.Group>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>Inserta una imagen para el item</Form.Label>
+            <Form.Control type="file" />
+          </Form.Group>
+          <Button type="submit">Crear ítem</Button>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Cerrar</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default AddItem;
